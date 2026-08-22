e9d447e3a64c09f736a8c94bf961c4c7a6db8188
ttt-yyy-111 <847187284tina@gmail.com>
2026-08-23 04:34:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3005,7 +3005,7 @@ function AnimatedSidebarUnderline({ children, active = false, enabled = false })
     color: "inherit",
     textDecorationLine: "underline",
     textDecorationColor: "#000",
-    textDecorationThickness: "4px",
+    textDecorationThickness: "2px",
     textUnderlineOffset: "2px",
     textDecorationSkipInk: "all",
     WebkitTextDecorationSkip: "ink",
