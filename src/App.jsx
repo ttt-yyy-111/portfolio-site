45db359ea7242a44408cb1dfecf645b438d81f05
ttt-yyy-111 <847187284tina@gmail.com>
2026-08-24 01:53:17 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3619,7 +3619,7 @@ function Portfolio() {
               borderRadius: 40,
             }}
           >
-            <div className="absolute inset-x-0 bottom-0 top-5">
+            <div className="absolute inset-x-0 bottom-0 top-10">
               {appRoot}
             </div>
           </div>
